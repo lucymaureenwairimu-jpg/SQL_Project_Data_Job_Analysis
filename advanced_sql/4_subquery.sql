0ffde0fe4fea78835690a5fb9d02757c1198190c
Lucymaureen Ndonga <lucymaureenwairimu@gmail.com>
2026-06-19 18:11:56 +0300

    first commit

---
+++ advanced_sql/4_subquery.sql
@@ -0,0 +1,26 @@
+SELECT *
+FROM (
+    SELECT * 
+    FROM job_postings_fact
+    WHERE EXTRACT(MONTH FROM job_posted_date) = 1
+) AS january_jobs;
+ -- SubQuery ends here 
+
+
+
+-- Another example
+
+SELECT 
+    company_id,
+    name AS company_name
+FROM company_dim
+WHERE company_id IN (
+    SELECT 
+        company_id
+    FROM
+        job_postings_fact
+    WHERE
+        job_no_degree_mention = TRUE
+    ORDER BY 
+        company_id
+    )
